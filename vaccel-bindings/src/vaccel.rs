f5d93c51b6f8bc41ce3d8451e8811dd27aa6ef4a
Babis Chalios <mail@bchalios.io>
2021-01-30 15:58:15 +0100

    vaccel-bindings: Make the API more idiomatic to Rust
    Since, every operation is performed on top of a session, make the API
    session specific. Also, avoid having to pass out arguments as mut
    references when calling the respective functions (we are not writing C,
    here). Instead, have the callee return them to the caller upon success.

--- vaccel-bindings/src/vaccel.rs
+++ vaccel-bindings/src/vaccel.rs
@@ -5,133 +5,78 @@ pub type Result<T> = std::result::Result<T, u32>;
 
 unsafe impl Send for vaccel_session {}
 
-pub fn new_session(
-    sess: &mut vaccel_session,
-    flags: u32
-    ) -> Result<()> {
-    let err = unsafe { vaccel_sess_init(sess, flags) as u32 };
+impl vaccel_session {
+    pub fn new(flags: u32) -> Result<vaccel_session> {
+        let mut sess = vaccel_session::default();
 
-    if err == VACCEL_OK {
-        Ok(())
-    } else {
-        Err(err)
+        match unsafe { vaccel_sess_init(&mut sess, flags) as u32 } {
+            VACCEL_OK => Ok(sess),
+            err => Err(err),
+        }
     }
-}
 
-pub fn close_session(sess: &mut vaccel_session) -> Result<()> {
-    let err = unsafe { vaccel_sess_free(sess) as u32 };
-
-    if err == VACCEL_OK {
-        Ok(())
-    } else {
-        Err(err)
+    pub fn close(&mut self) -> Result<()> {
+        match unsafe { vaccel_sess_free(self) as u32 } {
+            VACCEL_OK => Ok(()),
+            err => Err(err)
+        }
     }
-}
 
-pub fn noop(sess: &mut vaccel_session) -> Result<()> {
-    let err = unsafe { vaccel_noop(sess) as u32 };
-
-    if err == VACCEL_OK {
-        Ok(())
-    } else {
-        Err(err)
+    pub fn noop(&mut self) -> Result<()> {
+        match unsafe { vaccel_noop(self) as u32 } {
+            VACCEL_OK => Ok(()),
+            err => Err(err)
+        }
     }
-}
 
-pub fn sgemm(
-    sess: &mut vaccel_session,
-    k: u32,
-    m: u32,
-    n: u32,
-    a: &mut [f32],
-    b: &mut [f32],
-    c: &mut [f32],
-) -> Result<()> {
-    let err = unsafe {
-        vaccel_sgemm(
-            sess,
-            k, m, n,
-            a.len() as u64,
-            b.len() as u64,
-            c.len() as u64,
-            a.as_mut_ptr(),
-            b.as_mut_ptr(),
-            c.as_mut_ptr(),
-        ) as u32
-    };
+    pub fn image_classification(&mut self, img: &[u8]) -> Result<(Vec<u8>, Vec<u8>)> {
+        let mut tags = vec![0; 1024];
+        let mut out_img = vec![0; 1024];
 
-    if err == VACCEL_OK {
-        Ok(())
-    } else {
-        Err(err)
+        match unsafe {
+            vaccel_image_classification(
+                self,
+                img.as_ptr() as *mut c_void,
+                tags.as_mut_ptr(),
+                out_img.as_mut_ptr(),
+                img.len() as u64,
+                tags.len() as u64,
+                out_img.len() as u64) as u32
+        } {
+            VACCEL_OK => Ok((tags, out_img)),
+            err => Err(err),
+        }
     }
-}
 
-pub fn image_classification(
-    sess: &mut vaccel_session,
-    img: &[u8],
-    out_text: &mut [u8],
-    out_imgname: &mut [u8],
-) -> Result<()> {
-    let err = unsafe {
-        vaccel_image_classification(
-            sess,
-            img.as_ptr() as *mut c_void,
-            out_text.as_mut_ptr(),
-            out_imgname.as_mut_ptr(),
-            img.len() as u64,
-            out_text.len() as u64,
-            out_imgname.len() as u64,
-        ) as u32
-    };
+    pub fn image_detection(&mut self, img: &mut [u8]) -> Result<Vec<u8>> {
+        let mut out_img = vec![0; img.len()];
 
-    if err == VACCEL_OK {
-        Ok(())
-    } else {
-        Err(err)
+        match unsafe {
+            vaccel_image_detection(
+                self,
+                img.as_mut_ptr() as *mut c_void,
+                out_img.as_mut_ptr(),
+                img.len() as u64,
+                out_img.len() as u64) as u32
+        } {
+            VACCEL_OK => Ok(out_img),
+            err => Err(err)
+        }
     }
-}
 
-pub fn image_detection(
-    sess: &mut vaccel_session,
-    img: &mut [u8],
-    out_imgname: &mut [u8],
-) -> Result<()> {
-    let err = unsafe {
-        vaccel_image_detection(
-            sess,
-            img.as_mut_ptr() as *mut c_void,
-            out_imgname.as_mut_ptr(),
-            img.len() as u64,
-            out_imgname.len() as u64
-        ) as u32
-    };
+    pub fn image_segmentation(&mut self, img: &mut [u8]) -> Result<Vec<u8>> {
+        let mut out_img = vec![0; img.len()];
 
-    if err == VACCEL_OK {
-        Ok(())
-    } else {
-        Err(err)
-    }
-}
-
-pub fn image_segmentation(
-    sess: &mut vaccel_session,
-    img: &mut [u8],
-    out_imgname: &mut [u8],
-) -> Result<()> {
-    let err = unsafe {
-        vaccel_image_segmentation(
-            sess,
-            img.as_mut_ptr() as *mut c_void,
-            out_imgname.as_mut_ptr(),
-            img.len() as u64,
-            out_imgname.len() as u64
-        ) as u32
-    };
-
-    if err == VACCEL_OK {
-        Ok(())
-    } else {
-        Err(err)
+        match unsafe {
+            vaccel_image_segmentation(
+                self,
+                img.as_mut_ptr() as *mut c_void,
+                out_img.as_mut_ptr(),
+                img.len() as u64,
+                out_img.len() as u64) as u32
+        } {
+            VACCEL_OK => Ok(out_img),
+            err => Err(err)
+        }
     }
 }
