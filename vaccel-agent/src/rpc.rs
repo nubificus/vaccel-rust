b2371e361115febe0c11195c95756bb97e522b71
Babis Chalios <mail@bchalios.io>
2021-01-31 19:41:36 +0100

    vaccel-agent: Adapt to new vaccel API
    [vaccel-bindings](https://github.com/bchalios/vaccel-bindings) has been
    restructured to offer a more Rust idiomatic API. Modify the agent to
    the new API. This includes keeping track of the session objects, in a
    HashMap, so we do not have to create dummy objects with every request.
    
    Signed-off-by: Babis Chalios <mail@bchalios.io>

--- vaccel-agent/src/rpc.rs
+++ vaccel-agent/src/rpc.rs
@@ -1,4 +1,7 @@
-use std::sync::{Arc};
+use std::sync::{Arc, Mutex};
+use std::collections::HashMap;
+
+use vaccel_bindings::vaccel_session;
 
 use protocols::agent::{
     CreateSessionRequest, CreateSessionResponse,
@@ -6,17 +9,26 @@ use protocols::agent::{
     ImageClassificationRequest, ImageClassificationResponse
 };
 
-#[derive(Clone)]
-pub struct Agent {
-}
 
 fn ttrpc_error(code: ttrpc::Code, msg: String) -> ttrpc::error::Error {
     ttrpc::Error::RpcStatus(ttrpc::error::get_status(code, msg))
 }
 
+#[derive(Clone)]
+pub struct Agent {
+    sessions: Arc<Mutex<HashMap<u32, vaccel_session>>>,
+}
+
+unsafe impl Sync for Agent {}
+unsafe impl Send for Agent {}
+
 pub fn start(server_address: &str) -> ttrpc::Server {
-    let vaccel_agent = Box::new(Agent{})
-        as Box<dyn protocols::agent_ttrpc::VaccelAgent + Send + Sync>;
+    let vaccel_agent =
+        Box::new(
+            Agent {
+                sessions: Arc::new(Mutex::new(HashMap::new()))
+            }
+        ) as Box<dyn protocols::agent_ttrpc::VaccelAgent + Send + Sync>;
 
     let agent_worker = Arc::new(vaccel_agent);
 
@@ -38,12 +50,14 @@ impl protocols::agent_ttrpc::VaccelAgent for Agent {
         _ctx: &::ttrpc::TtrpcContext,
         req: CreateSessionRequest
     ) -> ttrpc::Result<CreateSessionResponse> {
-        let mut sess: vaccel_bindings::vaccel_session = Default::default();
-        match vaccel_bindings::new_session(&mut sess, req.flags) {
+        match vaccel_session::new(req.flags) {
             Err(e) => Err(ttrpc_error(ttrpc::Code::INTERNAL, e.to_string())),
-            Ok(()) => {
+            Ok(sess) => {
                 let mut resp = CreateSessionResponse::new();
                 resp.session_id = sess.session_id;
+
+                let mut s = self.sessions.lock().unwrap();
+                s.insert(sess.session_id, sess);
 
                 println!("Created session {:?}", sess.session_id);
 
@@ -57,10 +71,14 @@ impl protocols::agent_ttrpc::VaccelAgent for Agent {
         _ctx: &::ttrpc::TtrpcContext,
         req: DestroySessionRequest
     ) -> ttrpc::Result<VaccelEmpty> {
-        let mut sess: vaccel_bindings::vaccel_session = Default::default();
-        sess.session_id = req.session_id;
+        let mut s = self.sessions.lock().unwrap();
+        let sess = match s.get_mut(&req.session_id) {
+            Some(sess) => sess,
+            None => return Err(ttrpc_error(ttrpc::Code::UNAVAILABLE, "Unknown session".to_string())),
+        };
+
         println!("Destroying session {:?}", sess.session_id);
-        match vaccel_bindings::close_session(&mut sess) {
+        match sess.close() {
             Err(e) => Err(ttrpc_error(ttrpc::Code::INTERNAL, e.to_string())),
             Ok(()) => {
                 println!("Destroyed session {:?}", req.session_id);
@@ -74,21 +92,21 @@ impl protocols::agent_ttrpc::VaccelAgent for Agent {
         _ctx: &::ttrpc::TtrpcContext,
         req: ImageClassificationRequest
     ) -> ttrpc::Result<ImageClassificationResponse> {
-        let mut sess: vaccel_bindings::vaccel_session = Default::default();
-        let mut tags = vec![0; 1024];
-        let mut image_path = vec![0; 1024];
+        let mut s = self.sessions.lock().unwrap();
+        let sess = match s.get_mut(&req.session_id) {
+            Some(sess) => sess,
+            None => return Err(ttrpc_error(ttrpc::Code::UNAVAILABLE, "Unknown session".to_string())),
+        };
 
-        sess.session_id = req.session_id;
         println!("session:{:?} Image classification", sess.session_id);
-        match vaccel_bindings::image_classification(
-        &mut sess, &req.image, &mut tags, &mut image_path) {
+        match sess.image_classification(&req.image) {
             Err(e) => {
                 println!("Could not perform classification");
                 Err(ttrpc_error(ttrpc::Code::INTERNAL, e.to_string()))
             },
-            Ok(()) => {
+            Ok((tags, _)) => {
                 let mut resp = ImageClassificationResponse::new();
-                resp.tags.append(&mut tags);
+                resp.tags = tags;
                 Ok(resp)
             }
         }
