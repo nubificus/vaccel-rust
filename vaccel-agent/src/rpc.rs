a38ca77736f0bd3f974be8516628c89f5ce2c818
Babis Chalios <mail@bchalios.io>
2021-05-25 17:19:03 +0200

    vaccel-agent: Implement vAccel API for TensorFlow inference
    Implement the calls for loading a TensorFlow graph from a registered
    model and running inference on the graph using a set of inputs.
    
    Also:
    
    1. The API for creating sessions and resources does not return a `Box`
       anymore. We need to put these objects in a Box when necessary.
    2. We now use the `id()` method for retrieving the id of sessions and
       resources, which is not a `Result`. If the object does not exist
       (which should not happen in our case), we will get an invalid id.
    
    Signed-off-by: Babis Chalios <mail@bchalios.io>

--- vaccel-agent/src/rpc.rs
+++ vaccel-agent/src/rpc.rs
@@ -1,19 +1,49 @@
 use chashmap::*;
+use std::default::Default;
 use std::sync::Arc;
 
 use vaccel_bindings::resource::VaccelResource;
-use vaccel_bindings::{vaccel_id_t, vaccel_session, vaccel_tf_model};
+use vaccel_bindings::{
+    vaccel_id_t, vaccel_session, vaccel_tf_buffer, vaccel_tf_model, vaccel_tf_node,
+    vaccel_tf_tensor, VACCEL_EINVAL, VACCEL_OK,
+};
 
-use protocols::image::{ImageClassificationRequest, ImageClassificationResponse};
+use protocols::error::VaccelError;
 use protocols::resources::{
     CreateResourceRequest, CreateResourceRequest_oneof_model, CreateResourceResponse,
     CreateTensorflowModelRequest, RegisterResourceRequest, UnregisterResourceRequest,
 };
 use protocols::session::{CreateSessionRequest, CreateSessionResponse, DestroySessionRequest};
 use protocols::{agent::VaccelEmpty, resources::DestroyResourceRequest};
+use protocols::{
+    image::{ImageClassificationRequest, ImageClassificationResponse},
+    tensorflow::{
+        InferenceResult, TensorflowModelLoadRequest, TensorflowModelLoadResponse,
+        TensorflowModelRunRequest, TensorflowModelRunResponse,
+        TensorflowModelRunResponse_oneof_result,
+    },
+};
 
 fn ttrpc_error(code: ttrpc::Code, msg: String) -> ttrpc::error::Error {
     ttrpc::Error::RpcStatus(ttrpc::error::get_status(code, msg))
+}
+
+fn vaccel_ok() -> VaccelError {
+    VaccelError {
+        error_code: VACCEL_OK as i64,
+        ..Default::default()
+    }
+}
+
+fn vaccel_error(err: vaccel_bindings::Error) -> VaccelError {
+    let mut grpc_error = VaccelError::new();
+
+    match err {
+        vaccel_bindings::Error::Runtime(e) => grpc_error.set_error_code(e as i64),
+        vaccel_bindings::Error::InvalidArgument => grpc_error.set_error_code(VACCEL_EINVAL as i64),
+    }
+
+    grpc_error
 }
 
 #[derive(Clone)]
@@ -55,10 +85,10 @@ impl protocols::agent_ttrpc::VaccelAgent for Agent {
             Err(e) => Err(ttrpc_error(ttrpc::Code::INTERNAL, e.to_string())),
             Ok(sess) => {
                 let mut resp = CreateSessionResponse::new();
-                resp.session_id = sess.session_id;
+                resp.session_id = sess.id();
 
-                assert!(!self.sessions.contains_key(&sess.session_id));
-                self.sessions.insert_new(sess.session_id, sess);
+                assert!(!self.sessions.contains_key(&sess.id()));
+                self.sessions.insert_new(sess.id(), Box::new(sess));
 
                 println!("Created session {:?}", resp.session_id);
                 Ok(resp)
@@ -76,7 +106,7 @@ impl protocols::agent_ttrpc::VaccelAgent for Agent {
             "Unknown session".to_string(),
         ))?;
 
-        println!("Destroying session {:?}", sess.session_id);
+        println!("Destroying session {:?}", sess.id());
         match sess.close() {
             Err(e) => Err(ttrpc_error(ttrpc::Code::INTERNAL, e.to_string())),
             Ok(()) => {
@@ -96,7 +126,7 @@ impl protocols::agent_ttrpc::VaccelAgent for Agent {
             "Unknown Session".to_string(),
         ))?;
 
-        println!("session:{:?} Image classification", sess.session_id);
+        println!("session:{:?} Image classification", sess.id());
         match sess.image_classification(&req.image) {
             Err(e) => {
                 println!("Could not perform classification");
@@ -200,6 +230,89 @@ impl protocols::agent_ttrpc::VaccelAgent for Agent {
             Err(e) => Err(ttrpc_error(ttrpc::Code::INTERNAL, e.to_string())),
         }
     }
+
+    fn tensorflow_model_load(
+        &self,
+        _ctx: &ttrpc::TtrpcContext,
+        req: TensorflowModelLoadRequest,
+    ) -> ttrpc::Result<TensorflowModelLoadResponse> {
+        let mut resource = self.resources.get_mut(&req.model_id).ok_or(ttrpc_error(
+            ttrpc::Code::INVALID_ARGUMENT,
+            "Unknown TensorFlow model".to_string(),
+        ))?;
+
+        let mut sess = self.sessions.get_mut(&req.session_id).ok_or(ttrpc_error(
+            ttrpc::Code::INVALID_ARGUMENT,
+            "Unknown session".to_string(),
+        ))?;
+
+        let model = resource
+            .as_mut_any()
+            .downcast_mut::<vaccel_tf_model>()
+            .ok_or(ttrpc_error(
+                ttrpc::Code::INVALID_ARGUMENT,
+                format!("Resource {} is not a TensorFlow model", req.model_id),
+            ))?;
+
+        let mut resp = TensorflowModelLoadResponse::new();
+        let err = match model.load_graph(&mut sess) {
+            Ok(_) => vaccel_ok(),
+            Err(e) => vaccel_error(e),
+        };
+
+        resp.set_error(err);
+        Ok(resp)
+    }
+
+    fn tensorflow_model_run(
+        &self,
+        _ctx: &ttrpc::TtrpcContext,
+        mut req: TensorflowModelRunRequest,
+    ) -> ttrpc::Result<TensorflowModelRunResponse> {
+        let mut resource = self.resources.get_mut(&req.model_id).ok_or(ttrpc_error(
+            ttrpc::Code::INVALID_ARGUMENT,
+            "Unknown TensorFlow model".to_string(),
+        ))?;
+
+        let mut sess = self.sessions.get_mut(&req.session_id).ok_or(ttrpc_error(
+            ttrpc::Code::INVALID_ARGUMENT,
+            "Unknown session".to_string(),
+        ))?;
+
+        let model = resource
+            .as_mut_any()
+            .downcast_mut::<vaccel_tf_model>()
+            .ok_or(ttrpc_error(
+                ttrpc::Code::INVALID_ARGUMENT,
+                format!("Resource {} is not a TensorFlow model", req.model_id),
+            ))?;
+
+        let run_options: vaccel_tf_buffer = req.mut_run_options().as_mut_slice().into();
+
+        let in_nodes: Vec<vaccel_tf_node> =
+            req.mut_in_nodes().iter_mut().map(|e| e.into()).collect();
+
+        let in_tensors: Vec<vaccel_tf_tensor> =
+            req.mut_in_tensors().iter_mut().map(|e| e.into()).collect();
+
+        let out_nodes: Vec<vaccel_tf_node> =
+            req.mut_out_nodes().iter_mut().map(|e| e.into()).collect();
+
+        let response =
+            match model.inference(&mut sess, &run_options, &in_nodes, &in_tensors, &out_nodes) {
+                Ok((out_tensors, _)) => {
+                    let mut inference = InferenceResult::new();
+                    inference.set_out_tensors(out_tensors.into_iter().map(|e| e.into()).collect());
+                    TensorflowModelRunResponse_oneof_result::result(inference)
+                }
+                Err(e) => TensorflowModelRunResponse_oneof_result::error(vaccel_error(e)),
+            };
+
+        Ok(TensorflowModelRunResponse {
+            result: Some(response),
+            ..Default::default()
+        })
+    }
 }
 
 impl Agent {
@@ -211,14 +324,11 @@ impl Agent {
         match vaccel_tf_model::from_buffer(req.get_model()) {
             Ok(model) => {
                 let mut resp = CreateResourceResponse::new();
-                let id = model.id().ok_or(ttrpc_error(
-                    ttrpc::Code::INTERNAL,
-                    "Could not get model id".to_string(),
-                ))?;
-                resp.set_resource_id(id);
-                self.resources.insert_new(id, model);
 
-                println!("Created new TensorFlow model with id: {}", id);
+                resp.set_resource_id(model.id());
+                self.resources.insert_new(model.id(), Box::new(model));
+
+                println!("Created new TensorFlow model with id: {}", model.id());
                 Ok(resp)
             }
             Err(e) => {
