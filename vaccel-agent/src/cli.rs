932d8211e359eea5744e93a1e39566618bedd1f9
Kostis Papazafeiropoulos <papazof@gmail.com>
2023-02-02 20:45:56 +0200

    vaccel-agent: Add profiling implementation
    Signed-off-by: Kostis Papazafeiropoulos <papazof@gmail.com>

--- vaccel-agent/src/cli.rs
+++ vaccel-agent/src/cli.rs
@@ -1,8 +1,11 @@
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "vAccel Agent", about = "A vAccel agent that can respond to gRPC requests for acceleration requests")]
+#[structopt(
+    name = "vAccel Agent",
+    about = "A vAccel agent that can respond to gRPC requests for acceleration requests"
+)]
 pub struct VaccelAgentCli {
     #[structopt(short = "a", long = "server-address")]
-    pub uri: String
+    pub uri: String,
 }
