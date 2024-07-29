6a4dc68180dd34d1b9e1b41b62b114dd32dd2971
Kostis Papazafeiropoulos <papazof@gmail.com>
2024-07-29 18:53:11 +0000

    build(meson): Add initial implementation
    Signed-off-by: Kostis Papazafeiropoulos <papazof@gmail.com>

---
+++ scripts/cargo.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# SPDX-License-Identifier: Apache-2.0
+
+set -e
+
+CARGO_BIN=$1
+MESON_BUILDTYPE=$2
+CARGO_TOML=$3
+OUTPUT=$4
+OUTPUT_DIR=$5
+#RUSTC_FLAGS="${RUSTC_FLAGS} print=native-static-libs"
+shift 5
+
+case "${MESON_BUILDTYPE}" in
+*debug*) CARGO_BUILDTYPE="debug" ;;
+*)
+	CARGO_BUILDTYPE="release"
+	CARGO_BUILD_FLAG="--release"
+	;;
+esac
+
+${CARGO_BIN} build --manifest-path "${CARGO_TOML}" \
+	${CARGO_BUILD_FLAG+"${CARGO_BUILD_FLAG}"} "$@"
+
+cp "${CARGO_TARGET_DIR}/${CARGO_BUILD_TARGET}/${CARGO_BUILDTYPE}/${OUTPUT}" \
+	"${OUTPUT_DIR}/${OUTPUT}"
