1ecfbfbeaca975c1fe74431b78957cc9c9a00f35
Kostis Papazafeiropoulos <papazof@gmail.com>
2024-10-30 04:02:52 +0000

    fix(vaccel-bindings): Update examples with Session/Resource changes
    Update examples with `Session`/`Resource` object changes:
    - Remove `tf_saved_model` example since the resource-specific type has
      been removed
    - Rename `session.close()` to `session.release()`
    - Update resource-related functions with the new API
    
    PR: https://github.com/nubificus/vaccel-rust/pull/5
    Signed-off-by: Kostis Papazafeiropoulos <papazof@gmail.com>
    Reviewed-by: Anastassios Nanos <ananos@nubificus.co.uk>
    Approved-by: Anastassios Nanos <ananos@nubificus.co.uk>

--- vaccel-bindings/examples/torch_inference.rs
+++ vaccel-bindings/examples/torch_inference.rs
@@ -6,27 +6,36 @@ use env_logger::Env;
 use log::info;
 use std::path::PathBuf;
 use vaccel::{
+    ffi,
     ops::{torch, torch::InferenceArgs, InferenceModel},
-    resources::SingleModel,
-    Session,
+    Resource, Session,
 };
 
 fn main() -> utilities::Result<()> {
     env_logger::Builder::from_env(Env::default().default_filter_or("debug")).init();
+
+    // Create session
     let mut sess = Session::new(0)?;
     info!("New session {}", sess.id());
 
-    let path = PathBuf::from("./examples/files/torch/cnn_trace.pt");
-    let mut model = SingleModel::new().from_export_dir(&path)?;
-    info!("New saved model from export dir: {}", model.id());
+    let path = vec![PathBuf::from("./examples/files/torch/cnn_trace.pt")
+        .to_string_lossy()
+        .to_string()];
+    let mut model = Resource::new(&path, ffi::VACCEL_RESOURCE_MODEL)?;
+    info!("New model {}", model.as_ref().id());
 
-    // Register for the model
-    sess.register(&mut model)?;
-    info!("Registered model {} with session {}", model.id(), sess.id());
+    // Register model with session
+    model.as_mut().register(&mut sess)?;
+    info!(
+        "Registered model {} with session {}",
+        model.as_ref().id(),
+        sess.id()
+    );
 
     // Prepare data
     let run_options = torch::Buffer::new(&[]); // vaccel torch buffer with data and size
-                                               // TODO: in_tensor setting, use random inputs here, but should be images instead
+                                               // TODO: in_tensor setting, use random inputs here,
+                                               // but should be images instead
     let in_tensor = torch::Tensor::<f32>::new(&[3 * 224 * 224]).with_data(&[1.0; 3 * 224 * 224])?;
     info!("in_tensor dim: {}", in_tensor.nr_dims());
 
@@ -36,8 +45,8 @@ fn main() -> utilities::Result<()> {
     sess_args.add_input(&in_tensor);
     sess_args.set_nr_outputs(1);
 
-    let result = model.run(&mut sess, &mut sess_args)?;
-
+    // Run inference
+    let result = model.as_mut().run(&mut sess, &mut sess_args)?;
     match result.get_output::<f32>(0) {
         Ok(out) => {
             println!("Success");
@@ -50,9 +59,21 @@ fn main() -> utilities::Result<()> {
                 println!("dim[{}]: {}", i, out.dim(i as usize).unwrap());
             }
         }
-        Err(err) => println!("Torch JitLoadForward failed: '{}'", err),
+        Err(e) => println!("Torch JitLoadForward failed: '{}'", e),
     }
 
-    sess.close()?;
+    model.as_mut().unregister(&mut sess)?;
+    info!(
+        "Unregistered model {} from session {}",
+        model.as_ref().id(),
+        sess.id()
+    );
+
+    // No need to release model explicitly, `release()` will be run on drop
+    //model.as_mut().release()?;
+
+    // No need to release session explicitly, `release()` will be run on drop
+    //sess.release()?;
+
     Ok(())
 }
