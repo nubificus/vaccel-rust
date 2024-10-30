738a59cd77e6a167675d07daa118145c658fae91
Kostis Papazafeiropoulos <papazof@gmail.com>
2024-10-30 03:31:56 +0000

    fix(vaccel-bindings): Release vaccel session/resource on drop
    Implement Drop trait for resource/session bindings. Make sure that the
    respective `release()` functions have been called when
    `Resource`/`Session` objects are dropped.
    
    PR: https://github.com/nubificus/vaccel-rust/pull/5
    Signed-off-by: Kostis Papazafeiropoulos <papazof@gmail.com>
    Reviewed-by: Anastassios Nanos <ananos@nubificus.co.uk>
    Approved-by: Anastassios Nanos <ananos@nubificus.co.uk>

--- vaccel-bindings/src/session.rs
+++ vaccel-bindings/src/session.rs
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::{ffi, Error, Result, VaccelId};
+use log::warn;
 
 /// The vAccel session  type
 ///
@@ -19,7 +20,7 @@ impl Session {
     ///
     /// # Arguments
     ///
-    /// * `flags` - Flags for session creation. Currently ignored.
+    /// * `flags` - Flags for session creation.
     pub fn new(flags: u32) -> Result<Self> {
         let mut inner = ffi::vaccel_session::default();
 
@@ -34,7 +35,12 @@ impl Session {
         VaccelId::from(self.inner.id)
     }
 
-    /// update hint for session
+    /// Returns `true` if the session has been initialized
+    pub fn initialized(&self) -> bool {
+        self.id().has_id()
+    }
+
+    /// Update hint for session
     pub fn update(&mut self, flags: u32) {
         self.inner.hint = flags;
     }
@@ -43,6 +49,10 @@ impl Session {
     ///
     /// This will close an open session and consume it.
     pub fn release(&mut self) -> Result<()> {
+        if !self.initialized() {
+            return Err(Error::Uninitialized);
+        }
+
         match unsafe { ffi::vaccel_session_release(&mut self.inner) as u32 } {
             ffi::VACCEL_OK => Ok(()),
             err => Err(Error::Runtime(err)),
@@ -55,5 +65,13 @@ impl Session {
 
     pub(crate) fn inner_mut(&mut self) -> &mut ffi::vaccel_session {
         &mut self.inner
+    }
+}
+
+impl Drop for Session {
+    fn drop(&mut self) {
+        if self.initialized() && self.release().is_err() {
+            warn!("Could not release session");
+        }
     }
 }
